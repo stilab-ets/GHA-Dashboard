13bfa031b593128d2116cee08ca3fe4963fb5739
Gaubbe <gabriel12125@gmail.com>
2025-11-06 00:56:50 -0500

    feat: added separation into aggregation periods

---
+++ backend/analysis/aggregation.py
@@ -0,0 +1,46 @@
+from core.models import *
+
+from typing import AsyncIterator, AsyncGenerator
+from datetime import datetime as dt, date, timedelta
+
+@dataclass
+class AggregationFilters:
+    runInfo: RunInfo
+    aggregationPeriod: AggregationPeriod
+    startDate: date
+    endDate: date
+
+def period_bounds_from_date(date: date, aggregationPeriod: AggregationPeriod) -> tuple[datetime, datetime]:
+    if aggregationPeriod == "day":
+        periodStart = dt(date.year, date.month, date.day)
+        periodEnd = periodStart + timedelta(days=1)
+        return (periodStart, periodEnd)
+    elif aggregationPeriod == "week":
+        calendar = date.isocalendar()
+        periodStart = dt.fromisocalendar(calendar.year, calendar.week, 1)
+        periodEnd = periodStart + timedelta(weeks=1)
+        return (periodStart, periodEnd)
+    elif aggregationPeriod == "month":
+        periodStart = dt(date.year, date.month, 1)
+        if date.month == 12:
+            periodEnd = dt(date.year + 1, 1, 1)
+        else:
+            periodEnd = dt(date.year, date.month + 1, 1)
+        return (periodStart, periodEnd)
+
+async def separate_into_periods(runs: AsyncIterator[RawData], aggregationPeriod: AggregationPeriod) -> AsyncGenerator[list[RawData], None]:
+    result = []
+    periodStart: datetime | None = None
+    periodEnd: datetime | None = None
+
+    async for run in runs:
+        if periodStart == None or periodEnd == None:
+            periodStart, periodEnd = period_bounds_from_date(run.created_at.date(), aggregationPeriod)
+            result.append(run)
+        else:
+            if run.created_at < periodStart or run.created_at >= periodEnd:
+                yield result
+                periodStart, periodEnd = period_bounds_from_date(run.created_at.date(), aggregationPeriod)
+                result = [run]
+            else:
+                result.append(run)
