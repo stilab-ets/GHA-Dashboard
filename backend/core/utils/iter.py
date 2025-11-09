fe2e86ab2b56aad6509b626768c680c4ce7583be
Gaubbe <gabriel12125@gmail.com>
2025-11-08 23:40:29 -0500

    feat: added async_chain

--- backend/core/utils/iter.py
+++ backend/core/utils/iter.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Generic, Iterable, AsyncIterator
+from typing import TypeVar, Generic, Iterable, AsyncIterable, AsyncIterator
 
 T = TypeVar('T') # Represents the yield type of the iterator
 
@@ -20,4 +20,26 @@ async def to_async_iter(sync_iter: Iterable[T]) -> AsyncIterator[T]:
         The transformed, now asynchronous iterator.
     """
     for item in sync_iter:
+        yield item
+
+async def async_chain(first: AsyncIterable[T], second: AsyncIterable[T]) -> AsyncIterator[T]:
+    """
+    Iterator that exhausts the first iterator, then exhausts the second
+    iterator.
+
+    Type Params:
+        T: The yield type of both input iterators, and the output iterator.
+
+    Args:
+        first (AsyncIterable[T]): The first iterator to be exhausted.
+        second (AsyncIterable[T]): The second iterator to be exhausted.
+
+    Returns:
+        An iterator that will yield all the values from the first one,
+        followed by all the values in the second one.
+    """
+
+    async for item in first:
+        yield item
+    async for item in second:
         yield item
