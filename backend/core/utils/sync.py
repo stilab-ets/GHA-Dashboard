5266f745d7893f976fea4b22f055b8699441858f
Gaubbe <gabriel12125@gmail.com>
2025-11-10 00:15:15 -0500

    feat: added a way to cancel async_tail

--- backend/core/utils/sync.py
+++ backend/core/utils/sync.py
@@ -1,8 +1,8 @@
 import os
 import asyncio
-from typing import AsyncIterator, Callable, TextIO
+from typing import AsyncIterator, TextIO
 
-async def async_tail(file: TextIO) -> AsyncIterator[str]:
+async def async_tail(file: TextIO, cancellation: asyncio.Event | None = None) -> AsyncIterator[str]:
     """
     Returns an asynchronous iterator yielding each new line added to a file.
 
@@ -16,13 +16,13 @@ async def async_tail(file: TextIO) -> AsyncIterator[str]:
     file.seek(0, os.SEEK_END)
 
     def _sync_get_new_line():
-        while True:
+        while cancellation == None or not cancellation.is_set():
             new_line = file.readline()
 
             if new_line:
                 return new_line.strip()
 
-    while True:
+    while cancellation == None or not cancellation.is_set():
         line = await asyncio.to_thread(_sync_get_new_line)
         if line:
             yield line
